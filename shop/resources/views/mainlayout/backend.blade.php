8a350de0b84f28dd91735765ac6619c3dfc179a1
wachirapong <oriounited@gmail.com>
2015-10-26 14:02:58 +0700

    Update หลังบ้าน
    เชื่อมไฟล์ php ส่วนต่างๆ และหน้าตาหลังบ้าน

---
+++ shop/resources/views/mainlayout/backend.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <title>I Love Accessory - @yield('title')</title>
+
+  <!-- Bootstrap -->
+  <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="bootstrap/css/dashboard.css" rel="stylesheet">
+
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
+    </head>
+    <body>
+
+      @extends('admin.headbar')
+      @section('headbar')
+      @parent
+      @show
+
+      <div class="container-fluid">
+      <div class="row">
+      @extends('admin.sidebar')
+      @section('sidebar')
+      @parent
+      @show
+
+      <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+        @yield('content')
+      </div>
+      </div>
+      </div>
+
+      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+      <!-- Include all compiled plugins (below), or include individual files as needed -->
+      <script src="bootstrap/js/bootstrap.min.js"></script>
+        <script src="bootstrap/js/backend.js"></script>
+
+    </body>
+    </html>
